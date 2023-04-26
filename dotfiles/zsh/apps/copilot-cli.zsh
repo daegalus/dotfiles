50f9e90529103f3db8946a8db7abf63541ccd404
Yulian Kuncheff <yulian@unity3d.com>
2023-04-26 14:20:15 -0700

    Work nix

---
+++ dotfiles/zsh/apps/copilot-cli.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+### Github CoPilot CLI
+# Sets up the alias for the github copilot cli
+eval "$(github-copilot-cli alias -- "$0")"
--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -19,6 +19,12 @@
       home-manager.lib.homeManagerConfiguration {
         pkgs = nixpkgs.legacyPackages.${arch};
         modules = [ ./home.nix ];
+      };
+
+    homeConfigurations."yulian@lx1e" =
+      home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${arch};
+        modules = [ ./work.nix ];
       };
     };
 }
