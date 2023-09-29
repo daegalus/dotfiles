16ab4b0eebf2c6f121f6d79607fbb563ed114521
Yulian Kuncheff <yulian@kuncheff.com>
2023-09-28 22:50:56 -0700

    Not needed on Bazzite

--- work.nix
+++ work.nix
@@ -8,6 +8,11 @@
     userEmail = "yulian@unity3d.com";
   };
 
+  programs.zsh = {
+    initExtra = "source ~/.nix-profile/etc/profile.d/nix.sh" + "\n" +
+      "source ~/.nix-profile/etc/profile.d/nix-daemon.sh"
+  }
+
   home.sessionVariables = {
     # gcloud
     USE_GKE_GCLOUD_AUTH_PLUGIN="true";
