97600961893f621bb45ef8466619673c10afa276
Yulian Kuncheff <yulian@kuncheff.com>
2024-07-15 12:15:01 -0700

    Updates

--- .dotter/pre_deploy.sh
+++ .dotter/pre_deploy.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+
+SKIP_HOMEBREW=${SKIP_HOMEBREW:-"false"}
+
+if [[ $SKIP_HOMEBREW != "false" ]]; then
+  echo "Skipping Homebrew installation"
+  exit 0
+fi
+
 brew bundle --file brew/Brewfile.base
 brew bundle --file brew/Brewfile.dev
 brew bundle --file brew/Brewfile.$HOSTNAME
