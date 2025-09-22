eda95852a96d4a80e73640352e4b1a8917b69f73
Yulian Kuncheff <yulian@kuncheff.com>
2025-09-22 13:44:47 +0200

    Some updates

--- .dotter/post_deploy.sh
+++ .dotter/post_deploy.sh
@@ -4,3 +4,8 @@ chmod 0600 ~/.ssh/config
 
 ln -sf /home/linuxbrew/.linuxbrew/share/fonts -t ~/.local/share
 fc-cache -f > /dev/null
+
+echo "Applying 1Password Polkit Action Policy..."
+if [ ! -f /etc/polkit-1/actions/com.1password.1Password.policy ]; then
+  brew info 1password-gui-linux --cask | grep "polkit" | grep -oP "\`.*\`" | sh
+fi
